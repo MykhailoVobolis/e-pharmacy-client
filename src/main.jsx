c889668ae6fed5be9840266010f60cba2faa4f24
MykhailoVobolis <mvobolis@gmail.com>
2024-12-04 15:15:29 +0200

    basic project setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import App from "./components/App/App.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </PersistGate>
+    </Provider> */}
   </React.StrictMode>
 );
